ac2fb7ceab7cce192a42039206aa3ae102f87b2f
scaleflower <scalesflowers@gmail.com>
2025-03-25 17:51:31 +0800

    Site updated: 2025-03-25 17:51:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/20/Test-pages/"];function toRandomPost(){
+var posts=["2025/03/25/低代码编程，恐怕不会成功/","2025/03/20/Test-pages/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
